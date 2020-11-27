1749429cd8c75111158ff8500e4c3daff51a7ddb
Stylianos Mavromatis <mail@smavromatis.com>
2020-11-27 18:00:30 -0500

    F Strings and python 2 string interpolation

---
+++ Python Chapters/f_strings.py
@@ -0,0 +1,9 @@
+age = 24
+print(age)
+print(type(age))
+
+print(f"{age} years old")
+
+print(f"Pi is approximately {22/7:12.50f}")
+pi = 22/7
+print(f"Pi is approximately {pi:12.50f}")
---
+++ Python Chapters/python_2_string_interpolation.py
@@ -0,0 +1,13 @@
+# Not to be used with python 3 since it may be removed in future versions.
+age = 24
+print("My age is %d years" % age)
+
+#for float you use %f and for strings you use %s
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s"%(age,major,6,minor))
+print("Pi is approx %f" %(22/7))
+print("Pi is approx %60.50f" %(22/7))
+
+# Link with string formatting
+# https://docs.pythin.org/2.4/lib/typesseq-strings.html
