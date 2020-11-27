9114cb5dbfe86a6d9037a96e11f5a538c511207d
Stylianos Mavromatis <mail@smavromatis.com>
2020-11-27 15:31:52 -0500

    Testing

---
+++ Python Chapters/sequence_operators.py
@@ -0,0 +1,17 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+print(string1 + string2 + string3 + string4 + string5)
+
+# Multiplication of strings
+print("Hello " * 5)
+print(string1*5)
+
+today = "friday"
+print("day" in today)       # true
+print("fri" in today)       # true
+print("thur" in today)      # false
+print("parrot" in "fjord")  # false
