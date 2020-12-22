51fa3ac963fba0729bc78abc5c41ebf2deced92e
Stylianos Mavromatis <mail@smavromatis.com>
2020-12-22 12:21:19 -0500

    Updated conditioning

--- Python Chapters/truefalse.py
+++ Python Chapters/truefalse.py
@@ -11,3 +11,9 @@ if day == "Saturday" and temperature > 27 or not raining:
     print("Go swimming")
 else:
     print("Learn Python")
+
+# Using parenthesis makes code easier to read especially when using AND and OR together
+if (day == "Saturday" and temperature > 27) or not raining:
+    print("Go swimming")
+else:
+    print("Learn Python")
