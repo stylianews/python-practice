87f6b22ebf90f837452d15e0dd108b5bac687dd6
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-08 09:55:55 -0500

    Random updated

--- Python Chapters/randommodule.py
+++ Python Chapters/randommodule.py
@@ -1,14 +1,16 @@
 import random
 
-answer = random.randint(1,10)
+highest = 1000
+answer = random.randint(1,highest)
 print(answer)   # TODO: Remove after testing
 
-print("Please guess number between 1 and 10: ")
+print("Please guess number between 1 and {}: ".format(highest))
 guess = 0
 while guess != answer:
     try:
         guess = int(input())
-
+        if guess == 0:
+            break
         if guess == answer:
             print("Well done!")
             break
