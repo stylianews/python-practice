583701d40a6097fd4e04cd076d9ee9fd60357c74
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-08 18:50:40 -0500

    Update

--- Python Chapters/guessingalgorithm.py
+++ Python Chapters/guessingalgorithm.py
@@ -6,7 +6,8 @@ input("Press enter to start")
 
 guesses = 1
 while True:
-    guess = low + (high-low) // 2
+    print("\tGuessing in the range of {} to {}".format(low, high))
+    guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Enter h or l or c if my guess is correct"
                      .format(guess)).casefold()
@@ -23,4 +24,5 @@ while True:
     else:
         print("Please enter h,l or c")
 
-    guesses = guesses + 1
+    # guesses = guesses + 1
+    guesses += 1
