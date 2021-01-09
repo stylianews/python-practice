f9bd902cd921525fd0c78eda57b3f4737c2442b9
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-09 12:28:42 -0500

    Using else to fix adventure.py problem

---
+++ Python Chapters/anotherelseprogram.py
@@ -0,0 +1,13 @@
+# Use of while loops in an example
+available_exits = ["north", "south", "east", "west"]
+
+chosen_exit = ""
+
+# The loop will repeat itself unless the user inputs an available exit
+while chosen_exit not in available_exits:
+    chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() == "quit":
+        print("Game over")
+        break
+else:
+    print("arent you glad you got out of there")
