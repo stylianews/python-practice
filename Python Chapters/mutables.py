4499738872e03c67f60cf5d14d190e22ea63b8b1
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-09 16:31:34 -0500

    Update

--- Python Chapters/mutables.py
+++ Python Chapters/mutables.py
@@ -13,3 +13,12 @@ print(id(shopping_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+print(another_list)
+
+a = b = c = d = e = f = another_list
+print(a)
+
+print("Adding cream")
+b.append("cream")
+print(c)
+print(d)
