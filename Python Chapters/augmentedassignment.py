aaa270360e12ed149200c8d27c60bc6bd482a042
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-08 19:00:32 -0500

    Augmented Assignment

---
+++ Python Chapters/augmentedassignment.py
@@ -0,0 +1,20 @@
+x = 23
+
+x += 1
+print(x)
+
+x -= 4
+print(x)
+
+x *= 5
+print(x)
+
+x //= 4
+print(x)
+
+greeting = "Good "
+greeting += "morning "
+print(greeting)
+
+greeting *= 5
+print(greeting)
---
+++ Python Chapters/augmentedchallenge.py
@@ -0,0 +1,12 @@
+# For example, 5 * 8 was performed by adding 5 eight times
+# Use a for loop, and augmented assignment, to give the result of multiplying number by multiplier
+
+number = 5
+multiplier = 8
+answer = 0
+
+# add your loop after this comment
+for i in range(0, multiplier):
+    answer += number
+
+print(answer)
