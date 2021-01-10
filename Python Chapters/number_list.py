b8b3be9f5ebbcbc0a7038a60fbf90844bd77fa2b
Stylianos Mavromatis <mail@smavromatis.com>
2021-01-10 16:36:16 -0500

    Update

--- Python Chapters/number_list.py
+++ Python Chapters/number_list.py
@@ -5,3 +5,14 @@ print(min(even))
 print(max(even))
 print(min(odd))
 print(max(odd))
+
+print()
+# Length
+print(len(even))
+print(len(odd))
+
+print()
+print("mississippi".count("s"))
+print("mississippi".count("iss"))
+# No re-use
+print("mississippi".count("issi"))
